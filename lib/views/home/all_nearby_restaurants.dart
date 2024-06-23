25861c164ebc6c0554da85bc2e9a96fdc4005151
Khuzaima <khuzaimaansari2004@gmail.com>
2024-06-23 17:21:31 +0530

    Screens created for headings

--- lib/views/home/all_nearby_restaurants.dart
+++ lib/views/home/all_nearby_restaurants.dart
@@ -13,26 +13,28 @@ class AllNearbyRestaurants extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: kOffWhite,
-          title: ReusableText(
-            text: 'Nearby Restaurants',
-            style: appStyle(13, kGray, FontWeight.w600),
+      backgroundColor: kSecondary,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: kSecondary,
+        title: ReusableText(
+          text: 'Nearby Restaurants',
+          style: appStyle(13, kLightWhite, FontWeight.w600),
+        ),
+      ),
+      body: BackGroundContainer(
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.all(12.h),
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: List.generate(restaurants.length, (i) {
+              var restaurant = restaurants[i];
+              return RestaurentTile(restaurant: restaurant);
+            }),
           ),
         ),
-        body: BackGroundContainer(
-          color: Colors.white,
-          child: Padding(
-            padding: EdgeInsets.all(12.h),
-            child: ListView(
-              scrollDirection: Axis.vertical,
-              children: List.generate(restaurants.length, (i) {
-                var restaurant = restaurants[i];
-                return RestaurentTile(restaurant: restaurant);
-              }),
-            ),
-          ),
-        ));
+      ),
+    );
   }
 }
