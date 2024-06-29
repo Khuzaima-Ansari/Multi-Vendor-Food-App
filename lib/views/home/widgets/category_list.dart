d80029796478015020df23c7963b31b826cfd03b
Khuzaima <khuzaimaansari2004@gmail.com>
2024-06-30 01:01:09 +0530

    Integrated API for all categories

--- lib/views/home/widgets/category_list.dart
+++ lib/views/home/widgets/category_list.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodly/common/shimmers/categories_shimmer.dart';
-import 'package:foodly/constants/uidata.dart';
 import 'package:foodly/hooks/fetch_categories.dart';
 import 'package:foodly/models/categories.dart';
 
